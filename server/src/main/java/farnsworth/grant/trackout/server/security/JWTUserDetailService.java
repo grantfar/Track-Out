c744834e7bdc79f15396c8249a2a28074f7e0a6d
Grant Farnsworth <Grant.Farnsworth@sigma.local>
2022-09-23 11:20:55 -0400

    Worked on getting a database connection

---
+++ server/src/main/java/farnsworth/grant/trackout/server/security/JWTUserDetailService.java
@@ -0,0 +1,14 @@
+package farnsworth.grant.trackout.server.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class JWTUserDetailService implements UserDetailsService{
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+    
+}
---
+++ server/src/main/java/farnsworth/grant/trackout/server/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package farnsworth.grant.trackout.server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()
+        .antMatchers("/rest/user/login","/rest/user/register").permitAll()
+        .antMatchers("/rest/admin").hasAuthority("admin"));
+        return http.build();
+    }
+}
